9bb6951a9121f939cd5693d6572ff8581cb6da47
Zed <zedzedzednullnullnull@gmail.com>
2017-01-23 20:46:30 -0500

    Fixed both noted shortcomings - track numbers exist, and folder arguments mean something.

--- split.py
+++ split.py
@@ -1,12 +1,14 @@
 
-import multiprocessing
 import argparse
+import errno
+import multiprocessing
 import os
 import os.path
+import shutil
 import struct
 import subprocess
+import sys
 import tempfile
-import time
 import wave
 
 import musicbrainzngs as mb
@@ -15,22 +17,21 @@ import idgetter
 
 
 READBUF = 44100 * 60
-args = None
 
 def convert(fileinfo):
-    artist, album, trackname, trackfilename = fileinfo
+    artist, album, trackname, trackorder, trackfilename, trackdestination = fileinfo
     print(fileinfo)
     newname = '.'.join([os.path.splitext(os.path.split(trackfilename)[1])[0], "mp3"])
-    newpath = os.sep.join(('done', newname))
-    print('ffmpeg -i "{0}" -codec:a libmp3lame -qscale:a 2 -metadata album="{2}" -metadata artist="{3}" -metadata title="{4}" "{1}" '.format(trackfilename, newpath, album, artist, trackname))
-    os.system('ffmpeg -i "{0}" -codec:a libmp3lame -qscale:a 2 -metadata album="{2}" -metadata artist="{3}" -metadata title="{4}" "{1}" '.format(trackfilename, newpath, album, artist, trackname))
+    newpath = os.sep.join((trackdestination, newname))
+    print('ffmpeg -i "{0}" -codec:a libmp3lame -qscale:a 2 -metadata album="{2}" -metadata artist="{3}" -metadata title="{4}" -metadata track="{5}" "{1}" '.format(trackfilename, newpath, album, artist, trackname, trackorder))
+    os.system('ffmpeg -i "{0}" -codec:a libmp3lame -qscale:a 2 -metadata album="{2}" -metadata artist="{3}" -metadata title="{4}" -metadata track="{5}" "{1}" '.format(trackfilename, newpath, album, artist, trackname, trackorder))
     os.remove(trackfilename)
 
-def splittrack(filename):
+def splittrack(packarg):
+    args, filename = packarg
     newname = '.'.join([os.path.splitext(os.path.split(filename)[1])[0], "mp3"])
     with tempfile.NamedTemporaryFile(dir='tmp', suffix='.wav', delete=False) as tmpfile:
         tmp_file_name = tmpfile.name
-    newpath = os.sep.join((os.path.split(filename)[0], "conversions", newname))
     artist, album, tracklist = idgetter.get_track_list(newname)
     print(' '.join(['ffmpeg', '-i', filename, '-codec:a', 'pcm_s16le', tmp_file_name]))
     if tracklist is None:
@@ -76,7 +77,7 @@ def splittrack(filename):
                         tout.setframerate(samprate)
                         tout.writeframes(b''.join((struct.pack('<hh',smpl, smpr) for smpl, smpr in zip(chana[0:ender], chanb[0:ender]))))
                         tout.close()
-                        tracks.append((artist, album, trackname, trackfilename))
+                        tracks.append((artist, album, trackname, '%d/%d' % (j, len(tracklist)), trackfilename, args.destination))
                         j += 1
                         chana = chana[ender:]
                         chanb = chanb[ender:]
@@ -97,13 +98,14 @@ def splittrack(filename):
         tout.setframerate(samprate)
         tout.writeframes(b''.join((struct.pack('<hh',smpl, smpr) for smpl, smpr in zip(chana[0:ender], chanb[0:ender]))))
         tout.close()
-        tracks.append((artist, album, trackname, trackfilename))
+        tracks.append((artist, album, trackname, '%d/%d' % (j, len(tracklist)), trackfilename, args.destination))
     f.close()
     os.remove(tmp_file_name)
     print(tracks)
     list(map(convert, tracks))
     print("And that' it")
     #move source file to args.finished
+    shutil.move(filename, os.path.join(args.finished, filename))
     return
 
 
@@ -113,19 +115,33 @@ def get_media_paths(folder):
       if '.' in f and os.path.splitext(f)[1] in ('.mp3')]
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
+    #multiprocessing.freeze_support()
     parser = argparse.ArgumentParser(description='Convert media to media')
     parser.add_argument('-s', '--source', required=True, help='Source directory containing mp4 or webm files')
     parser.add_argument('-d', '--destination', required=True, help='Destination directory to place converted files')
     parser.add_argument('-f', '--finished', required=True, help='Directory to move source files after conversion')
     args = parser.parse_args()
+    wdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    args.finished = os.path.join(wdir, args.finished)
+    args.destination = os.path.join(wdir, args.destination)
+    args.source = os.path.join(wdir, args.source)
+    try:
+        os.makedirs(args.destination)
+    except OSError as ex:
+        if ex.errno != errno.EEXIST:
+            raise
+    try:
+        os.makedirs(args.finished)
+    except OSError as ex:
+        if ex.errno != errno.EEXIST:
+            raise
     folder = os.path.abspath(args.source)
     media = get_media_paths(folder)
     print(media)
     mb.set_useragent("Zed Track Splitter", "0.1", "http://gitgud.malvager.net/zed")
 
     splitpool = multiprocessing.Pool(processes=1)
-    splitpool.map(splittrack, media)
+    splitpool.map(splittrack, ((args, m) for m in media))
     splitpool.close() 
     splitpool.join()
 
